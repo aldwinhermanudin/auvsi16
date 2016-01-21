9292ac0330baf02fdc6e16ea0ec67effd4e1ae67
Arief Purnama Muharram <arief.purnamamuharram@yahoo.co.id>
2016-01-21 11:10:24 +0700

    new video_processing_thresholderProcessing node for processing video in the topic to produce thresholded video

---
+++ src/auvsi16/src/testing/video_processing_thresholderProcessing.cpp
@@ -0,0 +1,83 @@
+/****************************************************************************************************************
+ * Nama Program	:	video_processing_thresholderProcessing														*
+ * Last editor	:	Arief Purnama Muharram																		*
+ * Last edit	:	21/01/2016																					*
+ * Referensi	:	http://wiki.ros.org/image_transport/Tutorials/SubscribingToImages							*
+ * 					http://opencv-srf.blogspot.co.id/2010/09/object-detection-using-color-seperation.html		*
+ * **************************************************************************************************************/
+
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <cv_bridge/cv_bridge.h>
+#include "opencv2/imgproc/imgproc.hpp"
+
+using namespace std;
+using namespace cv;
+
+// Deklarasi variabel untuk nilai HSV
+	int iLowH = 0;
+	int iHighH = 179;
+
+	int iLowS = 0;
+	int iHighS = 255;
+
+	int iLowV = 0;
+	int iHighV = 255;
+
+void imageCallback(const sensor_msgs::ImageConstPtr& msg)
+{
+  try
+  {
+    cv::imshow("view", cv_bridge::toCvShare(msg, "bgr8")->image); // Tampilkan video yang berada di topik
+    cv::waitKey(30);
+    
+	namedWindow("Control", CV_WINDOW_AUTOSIZE); //create a window called "Control"
+
+	//Create trackbars in "Control" window
+	cvCreateTrackbar("LowH", "Control", &iLowH, 179); //Hue (0 - 179)
+	cvCreateTrackbar("HighH", "Control", &iHighH, 179);
+
+	cvCreateTrackbar("LowS", "Control", &iLowS, 255); //Saturation (0 - 255)
+	cvCreateTrackbar("HighS", "Control", &iHighS, 255);
+
+	cvCreateTrackbar("LowV", "Control", &iLowV, 255); //Value (0 - 255)
+	cvCreateTrackbar("HighV", "Control", &iHighV, 255);
+	
+	Mat imgOriginal = cv_bridge::toCvShare(msg, "bgr8")->image;
+	
+	Mat imgHSV;
+
+	cvtColor(imgOriginal, imgHSV, COLOR_BGR2HSV); //Convert the captured frame from BGR to HSV
+ 
+	Mat imgThresholded;
+
+	inRange(imgHSV, Scalar(iLowH, iLowS, iLowV), Scalar(iHighH, iHighS, iHighV), imgThresholded); //Threshold the image
+      
+	//morphological opening (remove small objects from the foreground)
+	erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+	dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) ); 
+
+	//morphological closing (fill small holes in the foreground)
+	dilate( imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) ); 
+	erode(imgThresholded, imgThresholded, getStructuringElement(MORPH_ELLIPSE, Size(5, 5)) );
+
+	imshow("Thresholded Image", imgThresholded); //show the thresholded image
+  }
+  catch (cv_bridge::Exception& e)
+  {
+    ROS_ERROR("Could not convert from '%s' to 'bgr8'.", msg->encoding.c_str());
+  }
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "video_processing_thresholderProcessing"); // Nama program: video_processing_thresholderProcessing
+  ros::NodeHandle nh;
+  cv::namedWindow("view");
+  cv::startWindowThread();
+  image_transport::ImageTransport it(nh);
+  image_transport::Subscriber sub = it.subscribe("AUVSI16/media/video", 1, imageCallback); // Topik subscribe: AUVSI16/media/video
+  ros::spin();
+  cv::destroyWindow("view");
+}
