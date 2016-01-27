464ffcd86a1722f6befa355c112f3c50701226f4
aldwinakbar <aldwinakbar@gmail.com>
2016-01-28 00:55:44 +0700

    [update] remove loop rate, changed variable, minor changes

--- src/auvsi16/src/example/video_publisher.cpp
+++ src/auvsi16/src/example/video_publisher.cpp
@@ -12,33 +12,28 @@
 
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "video_publisher"); // Nama program: video_publisher
+  ros::init(argc, argv, "video_feed"); // node name: video_publisher
   ros::NodeHandle nh;
   
   image_transport::ImageTransport it(nh);
-  image_transport::Publisher pub = it.advertise("auvsi16/video", 10000); // Topik publish: AUVSI16//media/video; Queue: 1000
+  image_transport::Publisher pub_video = it.advertise("auvsi16/video", 1); // topic: auvsi16/video; Queue: 1
 
-
-  int video0;
-  cv::VideoCapture cap(video0); // Ambil video dari file video0
-  
-  // Cek kamera; Error handling
+  cv::VideoCapture cap(0); 					// set video source to video0
+  // Camera Check; Error handling
   if(!cap.isOpened()) {
-	  ROS_ERROR ("Camera Error");	  
+	  ROS_ERROR ("Error opening camera.");	  
 	  return 1;
   }
   
   cv::Mat frame;
-  sensor_msgs::ImagePtr msg;
-
-  ros::Rate loop_rate(100);
+  sensor_msgs::ImagePtr image_msg;
+  
   while (nh.ok()) {
-    cap >> frame;
+    cap.read(frame);
     // Check if grabbed frame is actually full with some content
     if(!frame.empty()) {
-      msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", frame).toImageMsg();
-      pub.publish(msg);
-      cv::waitKey(1);
+      image_msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", frame).toImageMsg();
+      pub_video.publish(image_msg);
     }
 
     ros::spinOnce();
