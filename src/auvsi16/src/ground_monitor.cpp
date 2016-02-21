ad25c3521c3b646c22b05212769aa0cc8acb21ec
aldwinakbar <aldwinakbar@gmail.com>
2016-02-21 11:58:50 +0700

    added ground station node, video stream through ROS Topic

---
+++ src/auvsi16/src/ground_monitor.cpp
@@ -0,0 +1,33 @@
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <opencv2/highgui/highgui.hpp>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/CompressedImage.h>
+#include <iostream>
+
+using namespace std;
+cv::Mat img_stream ;
+
+void imageCallback(const sensor_msgs::CompressedImage& msg)
+{
+	img_stream = cv::imdecode(cv::Mat(msg.data),1);
+	if (!img_stream.empty()){
+		cv::imshow("Compressed Video", img_stream);
+	}
+		
+	if (cv::waitKey(30) == 27){
+		cout << "esc key is pressed by user" << endl;
+		ros::shutdown();
+	}
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "system_monitor");
+  ros::NodeHandle nh;
+  cv::namedWindow("Compressed Video", cv::WINDOW_NORMAL);
+  cv::startWindowThread();
+  ros::Subscriber sub = nh.subscribe("auvsi16/video/compressed", 1, imageCallback,ros::TransportHints().unreliable()); // setting queue to 1 remove delay in compressedImage data | also using UDP
+  ros::spin();
+  cv::destroyWindow("Compressed Video");
+}
