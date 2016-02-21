4a32045e8306ebd1e9c4f9f0cc99c7dc97e92b89
aldwinakbar <aldwinakbar@gmail.com>
2016-02-21 12:04:31 +0700

    repair override condition

--- src/auvsi16/src/motor_controller.cpp
+++ src/auvsi16/src/motor_controller.cpp
@@ -1,29 +1,41 @@
 #include "ros/ros.h"
 #include "mavros_msgs/OverrideRCIn.h"
 #include "auvsi16/overrideMotorRC.h"
+#include "std_msgs/Bool.h"
+#include <iostream>
 
 #define STEERING 0
 #define THROTTLE 2
 
+bool override_status = false;
 ros::Publisher pub_override_rc;
 mavros_msgs::OverrideRCIn override_out;
 void overrideInputCB(const auvsi16::overrideMotorRC& override_recv);
+void overrideStatusCB(const std_msgs::Bool& override_status_recv);
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "motor_control");
+  ros::init(argc, argv, "motor_controller");
   ros::NodeHandle n;
 
-  ros::Publisher pub_override_rc = n.advertise<mavros_msgs::OverrideRCIn>("/mavros/rc/override", 10);
-  ros::Subscriber sub_overrideMotorRC = n.subscribe("auvsi16/overrideMotorRC", 10, overrideInputCB);
+  pub_override_rc = n.advertise<mavros_msgs::OverrideRCIn>("/mavros/rc/override", 10);
+  ros::Subscriber sub_override_motor_rc = n.subscribe("auvsi16/overrideMotorRC", 1, overrideInputCB);
+  ros::Subscriber sub_override_status = n.subscribe("auvsi16/override_status", 1, overrideStatusCB);
   
   ros::spin();
   return 0;
 }
 
+void overrideStatusCB(const std_msgs::Bool& override_status_recv){
+	
+	override_status = override_status_recv.data;
+	
+}
+
 void overrideInputCB(const auvsi16::overrideMotorRC& override_recv){
    
-  if(override_recv.overrideRC){
+  // Drone control 
+  if(override_status){
 	for(int i=0; i < 8; i++) override_out.channels[i] = 0;	//Releases all Channels First
 	
 	override_out.channels[THROTTLE] = override_recv.throttle;
@@ -31,9 +43,9 @@ void overrideInputCB(const auvsi16::overrideMotorRC& override_recv){
     pub_override_rc.publish(override_out);
   }
   
-  else if(!override_recv.overrideRC){
-	for(int i=0; i < 8; i++) override_out.channels[i] = 0;	//Releases all Channels
+  // RC take control
+  else if(!override_status){
 	
-    pub_override_rc.publish(override_out);
+	ROS_WARN_STREAM( "[MC] Override Off") ;
   }
 }
